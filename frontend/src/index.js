fed1a5089fc0bb9ef32446d051fdf55c2de23e44
_tam <tamjid84@yahoo.com>
2018-06-18 19:15:52 +0600

    requirements

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,17 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
 import {
-    Row, Col, Card, CardBody, CardTitle,CardText
+    Row, Col, Card, CardBody, CardTitle, CardText, CardImg
 } from 'reactstrap'
 
 class ContentFeed extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            'items':[],
+            'items': [],
         }
     }
+
     componentDidMount() {
         this.getItems();
     }
@@ -21,41 +22,41 @@ class ContentFeed extends React.Component {
     getItems() {
         fetch('http://127.0.0.1:8000/api/item/')
             .then(results => results.json())
-            .then(results => this.setState({'items':results}))
+            .then(results => this.setState({'items': results}))
     }
 
     render() {
-        return(
+        return (
             <ul>
-                {this.state.items.map(function(item, index){
+                {this.state.items.map(function (item, index) {
                     return (
-                        <ContentItem item={item} key={index + 1} />
+                        <ContentItem item={item} key={index + 1}/>
                     )
                 })}
             </ul>
         )
     }
 }
-class ContentItem extends React.Component{
-    render(){
-        return(
-            <Row className="ContentItem">
-                <Col xs="6">
-                    <Card>
-                        <CardBody>
-                            <CardTitle>
-                                {this.props.item.title}
-                            </CardTitle>
-                            <CardText>
-                                {this.props.item.description}
-                            </CardText>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        )
-    }
-}
+
+const ContentItem = ({item}) => (
+    <Row className="ContentItem">
+        <Col xs="3"/>
+        <Col xs="12" sm="6">
+            <Card>
+                <CardImg src={item.image}/>
+                <CardBody>
+                    <CardTitle>
+                        {item.title}
+                    </CardTitle>
+                    <CardText>
+                        {item.description}
+                    </CardText>
+                </CardBody>
+            </Card>
+        </Col>
+    </Row>
+)
+
 ReactDOM.render(
     <ContentFeed/>,
     document.getElementById('root')
